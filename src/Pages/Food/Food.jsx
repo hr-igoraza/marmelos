4d708bae96db2c55e5fe1574de69bb10a619db71
ameentm24 <toameentm@gmail.com>
2025-03-10 17:50:39 +0530

    food page updates

--- src/Pages/Food/Food.jsx
+++ src/Pages/Food/Food.jsx
@@ -22,7 +22,7 @@ const Food = () => {
       {/* ========= */}
       <section className="facilities bg-primary-green p-5  md:rounded-[25px] rounded-[10px]  relative top-[-25px] md:py-20">
         <div className="flex flex-wrap container mx-auto ">
-          <div className="md:w-1/2 w-full md:px-10 ">
+          <div className="md:w-1/2 mx-auto w-full md:px-10 ">
             <div className="border-b border-[#F9D6D6] flex justify-between items-center py-5">
               <h3 className="text-[#F9D6D6] font-[700] fs-18">BREAKFAST</h3>
 
@@ -67,17 +67,17 @@ const Food = () => {
             </div>
             {/* ========== */}
           </div>
-          <div className="md:w-1/2 w-full relative">
+          {/* <div className="md:w-1/2 w-full relative">
             <h2 className="text-[#ffffff]  font-[700] fs-45  md:text-right text-center md:mt-0 mt-10">
               Experience the True Taste of
               <br /> Kerala’s Heritage at Our
               <br /> Resort
             </h2>
 
-            {/* <div className="border border-white px-3 py-1 rounded-[20px] w-fit absolute bottom-0 right-0">
+            <div className="border border-white px-3 py-1 rounded-[20px] w-fit absolute bottom-0 right-0">
               <p className="text-white fs-24 font-[700]">Explore more</p>
-            </div> */}
-          </div>
+            </div>
+          </div> */}
         </div>
       </section>
 
@@ -94,7 +94,7 @@ const Food = () => {
       <section className="magic container-full my-15">
         <div className="container mx-auto content-center md:h-[80vh] h-[50vh]">
           <h2 className="fs-65  text-white font-[700] md:text-right text-center">
-            "Experience the Magic of Chatti Choru A Taste of Kerala!
+          Experience the warmth of home-cooked meals at Marmelos Homestay
           </h2>
         </div>
       </section>
@@ -110,7 +110,7 @@ const Food = () => {
       <section className="symphony container-full my-15">
         <div className="container mx-auto content-center md:h-[90vh] h-[50vh]">
           <h2 className="fs-65  text-white font-[700] md:text-left text-center">
-            "Experience the Magic of Chatti Choru A Taste of Kerala!
+            Experience the Magic of Chatti Choru A Taste of Kerala!
           </h2>
         </div>
       </section>
